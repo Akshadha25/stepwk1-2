67e59b573c641f7bd458c78cd725b660e7fd5706
Akshadha25 <akshuswork@gmail.com>
2026-03-10 09:37:34 +0530

    Added Problem 2: Inventory Manager

---
+++ Problem2_InventoryManager/Problem2_InventoryManager.java
@@ -0,0 +1,6 @@
+// Problem 2: E-commerce Flash Sale Inventory Manager
+public class Problem2_InventoryManager {
+    public static void main(String[] args) {
+        System.out.println(""Hello Problem 2"");
+    }
+}
