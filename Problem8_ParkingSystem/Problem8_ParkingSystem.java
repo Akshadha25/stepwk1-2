ddb997e488c8bace49a35d5024476f736d6c1d6e
Akshadha25 <akshuswork@gmail.com>
2026-03-10 09:48:12 +0530

    Added Problem 8: Parking Lot Management

---
+++ Problem8_ParkingSystem/Problem8_ParkingSystem.java
@@ -0,0 +1,6 @@
+// Problem 8: Parking Lot Management with Open Addressing
+public class Problem8_ParkingSystem {
+    public static void main(String[] args) {
+        System.out.println(""Hello Problem 8"");
+    }
+}
