1b0f11fce8c610a7ba55779d72f32a0f2c3d8995
Akshadha25 <akshuswork@gmail.com>
2026-03-10 09:57:39 +0530

    Added Problem 2: Inventory Manager template

---
+++ Problem2_InventoryManager/Problem2_InventoryManager.java
@@ -0,0 +1,6 @@
+public class Problem2_InventoryManager {
+    public static void main(String[] args) {
+        System.out.println("Hello Problem 2: Inventory Manager");
+        // TODO: Implement inventory manager logic here
+    }
+}
