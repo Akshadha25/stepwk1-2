263cdf73ef16d94a2bdd9f26e1727d046badffe0
Akshadha25 <akshuswork@gmail.com>
2026-03-10 10:07:08 +0530

    Added Problem 4: Plagiarism Detector

--- Problem4_PlagiarismDetector/Problem4_PlagiarismDetector.java
+++ Problem4_PlagiarismDetector/Problem4_PlagiarismDetector.java
@@ -1,6 +1,21 @@
-// Problem 4: Plagiarism Detector
+import java.util.*;
+
 public class Problem4_PlagiarismDetector {
+
     public static void main(String[] args) {
-        System.out.println(""Hello Problem 4"");
+        String[] submissions = {
+            "public class A { public static void main(String[] args){ System.out.println(\"Hello\"); } }",
+            "public class B { public static void main(String[] args){ System.out.println(\"Hello\"); } }",
+            "public class C { public static void main(String[] args){ System.out.println(\"Hi\"); } }"
+        };
+
+        int n = submissions.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (submissions[i].equals(submissions[j])) {
+                    System.out.println("Plagiarism detected between submission " + i + " and " + j);
+                }
+            }
+        }
     }
 }
