d2d26cf967663528986ec81d18da10ec7aa5ac19
Akshadha25 <akshuswork@gmail.com>
2026-03-10 09:37:20 +0530

    Added Problem 1: Username Availability Checker

---
+++ Problem1_UsernameChecker/Problem1_UsernameChecker.java
@@ -0,0 +1,6 @@
+// Problem 1: Social Media Username Availability Checker
+public class Problem1_UsernameChecker {
+    public static void main(String[] args) {
+        System.out.println(""Hello Problem 1"");
+    }
+}
