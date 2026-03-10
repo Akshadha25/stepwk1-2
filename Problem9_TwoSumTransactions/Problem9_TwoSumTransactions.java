41e496c4bb5283682d1d4bb60ad46cda88716adf
Akshadha25 <akshuswork@gmail.com>
2026-03-10 10:20:21 +0530

    Added Problem 9: Two-Sum Transactions

--- Problem9_TwoSumTransactions/Problem9_TwoSumTransactions.java
+++ Problem9_TwoSumTransactions/Problem9_TwoSumTransactions.java
@@ -1,6 +1,31 @@
-// Problem 9: Two-Sum Problem Variants for Financial Transactions
+// Problem 9: Two-Sum Transactions
+import java.util.HashMap;
+import java.util.Map;
+
 public class Problem9_TwoSumTransactions {
+
+    // Find indices of two transactions that sum to the target
+    public static int[] twoSum(int[] transactions, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < transactions.length; i++) {
+            int complement = target - transactions[i];
+            if (map.containsKey(complement)) {
+                return new int[]{map.get(complement), i};
+            }
+            map.put(transactions[i], i);
+        }
+        return new int[]{-1, -1}; // No solution found
+    }
+
     public static void main(String[] args) {
-        System.out.println(""Hello Problem 9"");
+        int[] transactions = {20, 35, 15, 40, 50};
+        int target = 55;
+
+        int[] result = twoSum(transactions, target);
+        if (result[0] != -1) {
+            System.out.println("Transactions at indices " + result[0] + " and " + result[1] + " sum to " + target);
+        } else {
+            System.out.println("No two transactions sum to " + target);
+        }
     }
 }
