06cc3812f22db00580535ce67520d886104f5a88
Akshadha25 <akshuswork@gmail.com>
2026-03-10 09:38:07 +0530

    Added Problem 7: Autocomplete System

---
+++ Problem7_AutocompleteSystem/Problem7_AutocompleteSystem.java
@@ -0,0 +1,6 @@
+// Problem 7: Autocomplete System for Search Engine
+public class Problem7_AutocompleteSystem {
+    public static void main(String[] args) {
+        System.out.println(""Hello Problem 7"");
+    }
+}
