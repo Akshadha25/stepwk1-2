3164b90a65a9e2cb22fb1e5f880b7058d5cf1db3
Akshadha25 <akshuswork@gmail.com>
2026-03-10 09:49:19 +0530

    Added Problem 10: Multi-Level Cache System

---
+++ Problem10_MultiLevelCache/Problem10_MultiLevelCache.java
@@ -0,0 +1,6 @@
+// Problem 10: Multi-Level Cache System with Hash Tables
+public class Problem10_MultiLevelCache {
+    public static void main(String[] args) {
+        System.out.println(""Hello Problem 10"");
+    }
+}
