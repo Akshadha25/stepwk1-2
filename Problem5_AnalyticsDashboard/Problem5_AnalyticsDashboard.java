8495f915813a6691b0e4d88683a4a4090deeed04
Akshadha25 <akshuswork@gmail.com>
2026-03-10 09:28:32 +0530

    Added Problem 5: Analytics Dashboard

---
+++ Problem5_AnalyticsDashboard/Problem5_AnalyticsDashboard.java
@@ -0,0 +1,6 @@
+// Problem 5: Analytics Dashboard
+public class Problem5_AnalyticsDashboard {
+    public static void main(String[] args) {
+        System.out.println(""Hello Problem 5"");
+    }
+}
