9d269fbac3737df3d5d760fa51385b0eece88c95
Akshadha25 <akshuswork@gmail.com>
2026-03-10 10:09:48 +0530

    Added Problem 4: Plagiarism Detector

--- Problem4_PlagiarismDetector/Problem4_PlagiarismDetector.java
+++ Problem4_PlagiarismDetector/Problem4_PlagiarismDetector.java
@@ -1,21 +1,34 @@
+// Problem 4: Plagiarism Detector
 import java.util.*;
 
 public class Problem4_PlagiarismDetector {
 
-    public static void main(String[] args) {
-        String[] submissions = {
-            "public class A { public static void main(String[] args){ System.out.println(\"Hello\"); } }",
-            "public class B { public static void main(String[] args){ System.out.println(\"Hello\"); } }",
-            "public class C { public static void main(String[] args){ System.out.println(\"Hi\"); } }"
-        };
+    // Method to calculate similarity percentage
+    public static double calculateSimilarity(String text1, String text2) {
+        Set<String> words1 = new HashSet<>(Arrays.asList(text1.toLowerCase().split("\\s+")));
+        Set<String> words2 = new HashSet<>(Arrays.asList(text2.toLowerCase().split("\\s+")));
 
-        int n = submissions.length;
-        for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
-                if (submissions[i].equals(submissions[j])) {
-                    System.out.println("Plagiarism detected between submission " + i + " and " + j);
-                }
-            }
+        Set<String> intersection = new HashSet<>(words1);
+        intersection.retainAll(words2);
+
+        Set<String> union = new HashSet<>(words1);
+        union.addAll(words2);
+
+        return ((double) intersection.size() / union.size()) * 100;
+    }
+
+    public static void main(String[] args) {
+        String doc1 = "This is a sample document to check plagiarism detection";
+        String doc2 = "This document is a sample to detect plagiarism";
+
+        double similarity = calculateSimilarity(doc1, doc2);
+        System.out.println("Similarity: " + String.format("%.2f", similarity) + "%");
+
+        // Example: Flag if similarity > 50%
+        if (similarity > 50) {
+            System.out.println("Plagiarism likely detected!");
+        } else {
+            System.out.println("Documents are sufficiently different.");
         }
     }
 }
